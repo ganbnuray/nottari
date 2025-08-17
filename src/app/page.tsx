018bc1ed90f08915856fe52d70b3ad5385a48057
Ganbarova Nuray <ganbnuray@gmail.com>
2025-08-18 03:24:46 +0400

    feat: basic editor setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
-import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import DocumentsPage from "./documents/page";
 const Home = () => {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <Button>Click Me</Button>
+      Click{" "}
+      <Link href="/documents/123">
+        <span className="text-blue-500 underline">&nbsp;Here&nbsp;</span>
+      </Link>{" "}
+      to go to document id
     </div>
   );
 };
