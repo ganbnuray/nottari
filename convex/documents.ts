fd22b870d128be330365d0e6d5a02b8045f6ddd2
Ganbarova Nuray <ganbnuray@gmail.com>
2025-08-20 20:38:45 +0400

    feat: implement document creation

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,4 +1,28 @@
-import { query } from "./_generated/server";
+import { ConvexError, v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+import DocumentsPage from "@/app/documents/page";
+
+export const create = mutation({
+  args: {
+    title: v.optional(v.string()),
+    initialContent: v.optional(v.string()),
+  },
+  handler: async (ctx, args) => {
+    const user = await ctx.auth.getUserIdentity();
+
+    if (!user) {
+      throw new ConvexError("Unauthorized operation");
+    }
+
+    const documentId = await ctx.db.insert("documents", {
+      title: args.title ?? "Untitled",
+      ownerId: user.subject,
+      initialContent: args.initialContent,
+    });
+
+    return documentId;
+  },
+});
 
 export const get = query({
   handler: async (ctx) => {
